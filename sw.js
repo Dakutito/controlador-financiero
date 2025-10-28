d7441b583f0fa1e8a5ce22a7443cd63c1f20c1bd
Dakutito <122334969+Dakutito@users.noreply.github.com>
2025-10-28 01:50:44 -0500

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,55 +1,15 @@
-const CACHE_NAME = 'controlador-financiero-v1.0.0';
-const urlsToCache = [
-    './',
-    './index.html',
-    './style.css',
-    './code.js',
-    '/sw.js',
-    './manifest.json',
-    './img/portada.png',
-    './img/portada.png',
-    './img/portada.png',
-    'https://cdn.jsdelivr.net/npm/chart.js'
-];
-
-// Instalar el Service Worker
-self.addEventListener('install', function(event) {
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Cache abierto');
-            return cache.addAll(urlsToCache);
+        caches.open("app-cache").then(cache => {
+            return cache.addAll(["index.html", "historial.html", "script.js", "styles.css"]);
         })
     );
 });
 
-// Activar el Service Worker
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-        return Promise.all(
-        cacheNames.map(function(cacheName) {
-            if (cacheName !== CACHE_NAME) {
-            console.log('Eliminando cache viejo:', cacheName);
-            return caches.delete(cacheName);
-            }
-        })
-        );
-    })
-    );
-});
-
-// Interceptar requests
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", event => {
     event.respondWith(
-    caches.match(event.request)
-        .then(function(response) {
-        // Devuelve la respuesta del cache o haz fetch
-        if (response) {
-            return response;
-        }
-        return fetch(event.request);
-        }
-    )
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
